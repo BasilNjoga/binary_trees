5960230c857e60ab22a968c2d389a6665331944b
Basil Njoga <Basilnjoga@gmail.com>
2022-08-23 23:47:05 +0300

    check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,8 +14,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
 {
     binary_tree_t *binary_tree_node;
-    binary_tree_node = malloc(sizeof(binary_tree_t));
-    binary_tree_node->parent = parent;
+    binary_tree_node = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+    binary_tree_node->parent =  parent;
     binary_tree_node->n = value;
 
     return binary_tree_node;
