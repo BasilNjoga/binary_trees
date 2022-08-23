ee1eeddeb0bc0087e6ca3876fe2b576ba383f136
Basil Njoga <Basilnjoga@gmail.com>
2022-08-23 22:14:46 +0300

    check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,12 @@
 #include "binary_trees.h"
 
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value):
-    binary_tree_t temp =
-    binary_tree_t parent;
-    
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+{
+    binary_tree_t *binary_tree_node;
+    binary_tree_node = malloc(sizeof(binary_tree_t));
+    binary_tree_node->parent = parent;
+    binary_tree_node->n = value;
+
+    return binary_tree_node;
+}
