b5fcf6a021fe68ebcb006e66e36613903ae3f56d
Basil Njoga <Basilnjoga@gmail.com>
2022-08-23 22:25:30 +0300

    check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node: This is a function that creates a binary tree
+ * 
+ * @parent: This is a pointer to the parent of the node created
+ * @value: This is the integer value to be stored in the funciton
+ * 
+ * Return: binary_tree_t* 
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
 {
