9b548e406d4439c3e03a3074861d25cfae8e0e1f
Basil Njoga <Basilnjoga@gmail.com>
2022-08-25 01:04:00 +0300

    check

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i;
+printf("Hello world");
+/*
+for (i=0; i < 10; i++)
+{
+    printf("%d\n", i);
+    if ((i > 0) && (i < 2))
+    {
+        printf("This is me");
+    }
+}
+*/
+}
