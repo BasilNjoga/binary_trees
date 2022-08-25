9db39fe34c03ca6af545accd4346bf40d25fef66
Basil <basilnjoga@gmail.com>
2022-08-25 14:21:13 -0700

    check

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,19 +12,19 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+
+	size_t h;
 	size_t left;
 	size_t right;
-	size_t h;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	binary_tree_size(tree->left);
-	binary_tree_size(tree->right);
-	h++;
-	
-	return h;
-	
+	left = binary_tree_size(tree->left);
+	right =	binary_tree_size(tree->right);
+	h = 1 + left + right;
+
+	return (h);
 }
