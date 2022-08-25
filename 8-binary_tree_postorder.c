57325373c9f928a61509aae3746b03aa8d819a57
Basil <basilnjoga@gmail.com>
2022-08-25 12:23:38 -0700

    hceck

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,11 +14,11 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if tree == NULL
+	if (tree == NULL)
 	{
 		return;
 	}
-	binary_tree_postorder(tree->left);
-	binary_tree_postorder(tree->right);
-	func(tree);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
