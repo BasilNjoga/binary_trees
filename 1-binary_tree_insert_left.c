6b54d667814c4f9b06ada81fcd97f93647388ecc
Basil Njoga <Basilnjoga@gmail.com>
2022-08-25 23:29:05 +0300

    check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,29 +1,48 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
+ * binary_tree_insert_left - this function inserts a node as a left-child
  *
- * Return: Always 0 (Success)
+ * @parent: This is the parent node
+ *
+ * @value: This is the value to be inserted
+ *
+ * Return: binary_tree_t
  */
-int main(void)
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *root;
-    size_t height;
+	binary_tree_t *leftnode;
+	int temp;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
+	leftnode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if ((parent == NULL) || (leftnode == NULL))
+	{
+		return (NULL);
+	}
+	if (parent->left == NULL)
+	{
+		parent->left = leftnode;
+		parent->right = NULL;
+		leftnode->n = value;
+		leftnode->parent = parent;
+		leftnode->left = NULL;
+		leftnode->right = NULL;
+	}
+	else
+	{
+		temp = parent->left->n;
+		parent->left->n = value;
+		parent->left->left = leftnode;
+		parent->left->right = NULL;
+		leftnode->n = temp;
+		leftnode->parent = parent->left;
+		leftnode->left = NULL;
+		leftnode->right = NULL;
+	}
 
-    height = binary_tree_height(root);
-    printf("Height from %d: %lu\n", root->n, height);
-    height = binary_tree_height(root->right);
-    printf("Height from %d: %lu\n", root->right->n, height);
-    height = binary_tree_height(root->left->right);
-    printf("Height from %d: %lu\n", root->left->right->n, height);
-    return (0);
+
+	return (leftnode);
 }
