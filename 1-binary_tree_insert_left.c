0667514bd36509744bb0ac2f106719fd61ae5fb8
Basil <basilnjoga@gmail.com>
2022-08-25 05:04:41 -0700

    check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *leftnode;
-	binary_tree_t *temp;
+	int temp;
 
 	leftnode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 	if (parent == NULL)
@@ -34,12 +34,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		temp = parent->left;
-		parent->left = leftnode;
-		leftnode->left = temp;
-		leftnode->right = NULL;
-		leftnode->n = value;
-		leftnode->parent = parent;
+		temp = parent->left->n;
+		parent->left->n = value;
+		parent->left->left = leftnode;
+		leftnode->n = temp;
+		leftnode->parent = parent->left;
 	}
 
 
