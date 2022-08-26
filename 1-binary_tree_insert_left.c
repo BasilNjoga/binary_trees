e4503db4da40b78002faabb6d175f5def6d9a151
Basil <basilnjoga@gmail.com>
2022-08-26 13:38:55 -0700

    check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	leftnode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 	if ((parent == NULL) || (leftnode == NULL))
 	{
+		exit(0);
 		return (NULL);
 	}
 	if (parent->left == NULL)
