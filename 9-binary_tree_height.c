52cac06f1d63cfe44f9bf6f1661ff041614c27ef
Basil <basilnjoga@gmail.com>
2022-08-26 14:09:40 -0700

    check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,9 +18,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
-
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left > right)
